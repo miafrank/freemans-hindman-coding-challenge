f2588f90d020b93d329a8b80ae1dcb528e767010
Mia Frank <31712634+miafrank@users.noreply.github.com>
2025-02-07 16:10:04 -0600

    Add fixtures for tetsts

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+
+from app.deduplicator import Deduplicator
+from app.non_numeric_remover import NonNumericRemover
+from app.specialty_id_mapper import SpecialtyIDMapper
+
+
+@pytest.fixture
+def non_numeric_remover_instance():
+    return NonNumericRemover()
+
+
+@pytest.fixture
+def deduplicator_instance():
+    return Deduplicator()
+
+
+@pytest.fixture
+def specialty_id_mapper_instance(non_numeric_remover_instance, deduplicator_instance):
+    return SpecialtyIDMapper(non_numeric_remover_instance, deduplicator_instance)
